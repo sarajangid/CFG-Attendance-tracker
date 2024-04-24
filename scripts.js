248094b22ae797227ee89cc4e1238d3a777a17ba
Vinay Vellore <vinay_vellore@berkeley.edu>
2024-04-23 18:20:07 -0700

    Cleaned up website, added short fade in animation when opening about page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-console.log("from script file");
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to animate letters
+    function animateLetters(element) {
+        const text = element.textContent;
+        element.textContent = ''; // Clear the existing text
+        let delay = 0;
+
+        for (let letter of text) {
+            let span = document.createElement('span');
+            span.textContent = letter;
+            span.style.opacity = 0;
+            span.style.animation = `fadeInUp 0.5s ${delay}s forwards ease`;
+            element.appendChild(span);
+            delay += 0.05; // Increase delay for each letter to create a staggered effect
+        }
+    }
+
+    // WIP
+    const headers = document.querySelectorAll('.animate-letters');
+    headers.forEach(header => {
+        animateLetters(header);
+    });
+});
